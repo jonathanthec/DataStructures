0a6ff600cf88e3758be557fb321af233dd80ec1b
JonathanChen <jonny29009@yahoo.com>
2019-12-14 23:05:46 -0800

    create linkedlist files

---
+++ src/LinkedList/MyLinkedList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+/**
+ * @author Jonathan Chen
+ *
+ * Created in December, 2019
+ * Created to mimic the function of a linkedlist datastructure
+ **/
+
+import java.util.*;
+
+public class MyLinkedList {
+
+}
